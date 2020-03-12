dgraph-io/badger

Introduce StreamDone in Stream Writer (#1061)
This PR introduces, a way to tell StreamWriter to close a stream. Previously Streams were always open until Flush is called on StreamWriter. This resulted in memory utilisation, because of underlying TableBuilder to a sortedWriter. Also closing all sorted writer in single call resulted in 
more memory allocation(during Flush()). This can be useful in some case such as bulk loader in
Dgraph, where only one stream is active at a time.

pb/pb.pb.go
@@ -8,6 +8,7 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	io "io"
 	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -101,7 +102,9 @@ type KV struct {
 	ExpiresAt uint64 `protobuf:"varint,5,opt,name=expires_at,json=expiresAt,proto3" json:"expires_at,omitempty"`
 	Meta      []byte `protobuf:"bytes,6,opt,name=meta,proto3" json:"meta,omitempty"`
 	// Stream id is used to identify which stream the KV came from.
-	StreamId             uint32   `protobuf:"varint,10,opt,name=stream_id,json=streamId,proto3" json:"stream_id,omitempty"`
+	StreamId uint32 `protobuf:"varint,10,opt,name=stream_id,json=streamId,proto3" json:"stream_id,omitempty"`
+	// Stream done is to indicate end of stream.
+	StreamDone           bool     `protobuf:"varint,11,opt,name=stream_done,json=streamDone,proto3" json:"stream_done,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -187,6 +190,13 @@ func (m *KV) GetStreamId() uint32 {
 		return m.StreamId
 	}
 	return 0
+}
+
+func (m *KV) GetStreamDone() bool {
+	if m != nil {
+		return m.StreamDone
+	}
+	return false
 }
 
 type KVList struct {
@@ -632,46 +642,47 @@ func init() {
 func init() { proto.RegisterFile("pb.proto", fileDescriptor_f80abaa17e25ccc8) }
 
 var fileDescriptor_f80abaa17e25ccc8 = []byte{
-	// 611 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x53, 0xdd, 0x6e, 0x12, 0x41,
-	0x14, 0x66, 0x16, 0xba, 0xc0, 0xa1, 0xa5, 0xeb, 0x44, 0x9b, 0x35, 0x2a, 0xc1, 0x35, 0x26, 0xd8,
-	0x34, 0x5c, 0xb4, 0xc6, 0x1b, 0xaf, 0x28, 0xc5, 0x48, 0x68, 0x43, 0x32, 0x36, 0x4d, 0xe3, 0x0d,
-	0x19, 0x76, 0x0f, 0x65, 0xb3, 0xbf, 0xd9, 0x19, 0x36, 0xe5, 0x4d, 0x7c, 0x0f, 0x5f, 0xc2, 0x4b,
-	0x1f, 0xc1, 0xd4, 0x07, 0xd1, 0xcc, 0xec, 0xd2, 0x40, 0xf4, 0xee, 0x9c, 0xef, 0x3b, 0x73, 0xe6,
-	0xcc, 0xf7, 0xcd, 0x81, 0x46, 0x3a, 0xef, 0xa7, 0x59, 0x22, 0x13, 0x6a, 0xa4, 0x73, 0xe7, 0x3b,
-	0x01, 0x63, 0x72, 0x43, 0x2d, 0xa8, 0x06, 0xb8, 0xb6, 0x49, 0x97, 0xf4, 0xf6, 0x99, 0x0a, 0xe9,
-	0x53, 0xd8, 0xcb, 0x79, 0xb8, 0x42, 0xdb, 0xd0, 0x58, 0x91, 0xd0, 0x17, 0xd0, 0x5c, 0x09, 0xcc,
-	0x66, 0x11, 0x4a, 0x6e, 0x57, 0x35, 0xd3, 0x50, 0xc0, 0x15, 0x4a, 0x4e, 0x6d, 0xa8, 0xe7, 0x98,
-	0x09, 0x3f, 0x89, 0xed, 0x5a, 0x97, 0xf4, 0x6a, 0x6c, 0x93, 0xd2, 0x57, 0x00, 0x78, 0x9f, 0xfa,
-	0x19, 0x8a, 0x19, 0x97, 0xf6, 0x9e, 0x26, 0x9b, 0x25, 0x32, 0x90, 0x94, 0x42, 0x4d, 0x37, 0x34,
-	0x75, 0x43, 0x1d, 0xab, 0x9b, 0x84, 0xcc, 0x90, 0x47, 0x33, 0xdf, 0xb3, 0xa1, 0x4b, 0x7a, 0x07,
-	0xac, 0x51, 0x00, 0x63, 0xcf, 0xe9, 0x82, 0x39, 0xb9, 0xb9, 0xf4, 0x85, 0xa4, 0x47, 0x60, 0x04,
-	0xb9, 0x4d, 0xba, 0xd5, 0x5e, 0xeb, 0xd4, 0xec, 0xa7, 0xf3, 0xfe, 0xe4, 0x86, 0x19, 0x41, 0xee,
-	0x0c, 0xe0, 0xc9, 0x15, 0x8f, 0xfd, 0x05, 0x0a, 0x39, 0x5c, 0xf2, 0xf8, 0x0e, 0xbf, 0xa0, 0xa4,
-	0x27, 0x50, 0x77, 0x75, 0x22, 0xca, 0x13, 0x54, 0x9d, 0xd8, 0xad, 0x63, 0x9b, 0x12, 0xe7, 0x0f,
-	0x81, 0xf6, 0x2e, 0x47, 0xdb, 0x60, 0x8c, 0x3d, 0xad, 0x52, 0x8d, 0x19, 0x63, 0x8f, 0x9e, 0x80,
-	0x31, 0x4d, 0xb5, 0x42, 0xed, 0xd3, 0x97, 0xff, 0xf6, 0xea, 0x4f, 0x53, 0xcc, 0xb8, 0xf4, 0x93,
-	0x98, 0x19, 0xd3, 0x54, 0x49, 0x7a, 0x89, 0x39, 0x86, 0x5a, 0xb8, 0x03, 0x56, 0x24, 0xf4, 0x19,
-	0x98, 0x01, 0xae, 0xd5, 0x2b, 0x0b, 0xd1, 0xf6, 0x02, 0x5c, 0x8f, 0x3d, 0xfa, 0x11, 0x0e, 0x31,
-	0x76, 0xb3, 0x75, 0xaa, 0x8e, 0xcf, 0x78, 0x78, 0x97, 0x68, 0xdd, 0xda, 0xc5, 0xcc, 0xa3, 0x47,
-	0x6a, 0x10, 0xde, 0x25, 0xac, 0x8d, 0x3b, 0x39, 0xed, 0x42, 0xcb, 0x4d, 0xa2, 0x34, 0x43, 0xa1,
-	0xdd, 0x30, 0xf5, 0x7d, 0xdb, 0x90, 0xf3, 0x06, 0x9a, 0x8f, 0xc3, 0x51, 0x00, 0x73, 0xc8, 0x46,
-	0x83, 0xeb, 0x91, 0x55, 0x51, 0xf1, 0xc5, 0xe8, 0x72, 0x74, 0x3d, 0xb2, 0x88, 0x33, 0x86, 0xd6,
-	0x79, 0x98, 0xb8, 0xc1, 0x74, 0xb1, 0x10, 0x28, 0xff, 0xf3, 0x49, 0x8e, 0xc0, 0x4c, 0x34, 0xa7,
-	0x35, 0x38, 0x60, 0x65, 0xa6, 0x2a, 0x43, 0x8c, 0xcb, 0x77, 0xaa, 0xd0, 0xf9, 0x0a, 0x70, 0xcd,
-	0xe7, 0x21, 0x8e, 0x63, 0x0f, 0xef, 0xe9, 0x3b, 0xa8, 0x17, 0x95, 0x1b, 0x23, 0x0e, 0xd5, 0xa3,
-	0xb6, 0xee, 0x62, 0x1b, 0x9e, 0xbe, 0x86, 0xfd, 0x79, 0x98, 0x24, 0xd1, 0x6c, 0xe1, 0x87, 0x12,
-	0xb3, 0xf2, 0x3b, 0xb6, 0x34, 0xf6, 0x49, 0x43, 0x4e, 0x02, 0x8d, 0xe1, 0x12, 0xdd, 0x40, 0xac,
-	0x22, 0x7a, 0x0c, 0x35, 0xad, 0x15, 0xd1, 0x5a, 0x1d, 0xa9, 0xb6, 0x1b, 0xae, 0xaf, 0xa4, 0xc9,
-	0x7c, 0xb9, 0x8c, 0x98, 0xae, 0x51, 0x53, 0x8a, 0x55, 0xa4, 0x3b, 0xd6, 0x98, 0x0a, 0x9d, 0xb7,
-	0xd0, 0x7c, 0x2c, 0x2a, 0x54, 0x19, 0x9e, 0x9d, 0x0e, 0xad, 0x0a, 0xdd, 0x87, 0xc6, 0xed, 0xed,
-	0x67, 0x2e, 0x96, 0x1f, 0xde, 0x5b, 0xc4, 0x71, 0xa1, 0x7e, 0xc1, 0x25, 0x9f, 0xe0, 0x7a, 0xcb,
-	0x3d, 0xb2, 0xed, 0x1e, 0x85, 0x9a, 0xc7, 0x25, 0x2f, 0xa7, 0xd5, 0xb1, 0xfa, 0x3c, 0x7e, 0x5e,
-	0x2e, 0x8d, 0xe1, 0xe7, 0x6a, 0x29, 0xdc, 0x0c, 0xb9, 0x44, 0x4f, 0x2d, 0x85, 0x32, 0xbf, 0xca,
-	0x9a, 0x25, 0x32, 0x90, 0xc7, 0xcf, 0xa1, 0xbd, 0xeb, 0x32, 0xad, 0x43, 0x95, 0xa3, 0xb0, 0x2a,
-	0xe7, 0xd6, 0x8f, 0x87, 0x0e, 0xf9, 0xf9, 0xd0, 0x21, 0xbf, 0x1e, 0x3a, 0xe4, 0xdb, 0xef, 0x4e,
-	0x65, 0x6e, 0xea, 0x8d, 0x3e, 0xfb, 0x1b, 0x00, 0x00, 0xff, 0xff, 0x1d, 0x2f, 0x21, 0x79, 0xdd,
-	0x03, 0x00, 0x00,
+	// 631 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x53, 0xcf, 0x6e, 0xd3, 0x4e,
+	0x10, 0xce, 0x3a, 0xa9, 0x93, 0x4c, 0xda, 0x34, 0xbf, 0xd5, 0x8f, 0xca, 0x08, 0x08, 0xc6, 0x08,
+	0x29, 0x54, 0x55, 0x0e, 0x2d, 0xe2, 0xc2, 0x29, 0x4d, 0x83, 0x88, 0xd2, 0x2a, 0xd2, 0x52, 0x55,
+	0x15, 0x97, 0x68, 0x63, 0x4f, 0x1a, 0xcb, 0x7f, 0xd6, 0xb2, 0x37, 0x56, 0xf3, 0x26, 0x3c, 0x12,
+	0x47, 0x0e, 0x3c, 0x00, 0x2a, 0x0f, 0x02, 0xda, 0xb5, 0x53, 0x25, 0x82, 0xdb, 0xcc, 0xf7, 0xcd,
+	0xce, 0xce, 0x7e, 0xdf, 0x0e, 0x34, 0x92, 0x79, 0x3f, 0x49, 0x85, 0x14, 0xd4, 0x48, 0xe6, 0xce,
+	0x0f, 0x02, 0xc6, 0xe4, 0x86, 0x76, 0xa0, 0x1a, 0xe0, 0xda, 0x22, 0x36, 0xe9, 0xed, 0x33, 0x15,
+	0xd2, 0xff, 0x61, 0x2f, 0xe7, 0xe1, 0x0a, 0x2d, 0x43, 0x63, 0x45, 0x42, 0x9f, 0x41, 0x73, 0x95,
+	0x61, 0x3a, 0x8b, 0x50, 0x72, 0xab, 0xaa, 0x99, 0x86, 0x02, 0xae, 0x50, 0x72, 0x6a, 0x41, 0x3d,
+	0xc7, 0x34, 0xf3, 0x45, 0x6c, 0xd5, 0x6c, 0xd2, 0xab, 0xb1, 0x4d, 0x4a, 0x5f, 0x00, 0xe0, 0x7d,
+	0xe2, 0xa7, 0x98, 0xcd, 0xb8, 0xb4, 0xf6, 0x34, 0xd9, 0x2c, 0x91, 0x81, 0xa4, 0x14, 0x6a, 0xba,
+	0xa1, 0xa9, 0x1b, 0xea, 0x58, 0xdd, 0x94, 0xc9, 0x14, 0x79, 0x34, 0xf3, 0x3d, 0x0b, 0x6c, 0xd2,
+	0x3b, 0x60, 0x8d, 0x02, 0x18, 0x7b, 0xf4, 0x25, 0xb4, 0x4a, 0xd2, 0x13, 0x31, 0x5a, 0x2d, 0x9b,
+	0xf4, 0x1a, 0x0c, 0x0a, 0xe8, 0x42, 0xc4, 0xe8, 0xd8, 0x60, 0x4e, 0x6e, 0x2e, 0xfd, 0x4c, 0xd2,
+	0x23, 0x30, 0x82, 0xdc, 0x22, 0x76, 0xb5, 0xd7, 0x3a, 0x35, 0xfb, 0xc9, 0xbc, 0x3f, 0xb9, 0x61,
+	0x46, 0x90, 0x3b, 0x03, 0xf8, 0xef, 0x8a, 0xc7, 0xfe, 0x02, 0x33, 0x39, 0x5c, 0xf2, 0xf8, 0x0e,
+	0x3f, 0xa3, 0xa4, 0x27, 0x50, 0x77, 0x75, 0x92, 0x95, 0x27, 0xa8, 0x3a, 0xb1, 0x5b, 0xc7, 0x36,
+	0x25, 0xce, 0x6f, 0x02, 0xed, 0x5d, 0x8e, 0xb6, 0xc1, 0x18, 0x7b, 0x5a, 0xc6, 0x1a, 0x33, 0xc6,
+	0x1e, 0x3d, 0x01, 0x63, 0x9a, 0x68, 0x09, 0xdb, 0xa7, 0xcf, 0xff, 0xee, 0xd5, 0x9f, 0x26, 0x98,
+	0x72, 0xe9, 0x8b, 0x98, 0x19, 0xd3, 0x44, 0x69, 0x7e, 0x89, 0x39, 0x86, 0x5a, 0xd9, 0x03, 0x56,
+	0x24, 0xf4, 0x09, 0x98, 0x01, 0xae, 0x95, 0x0c, 0x85, 0xaa, 0x7b, 0x01, 0xae, 0xc7, 0x1e, 0xfd,
+	0x00, 0x87, 0x18, 0xbb, 0xe9, 0x3a, 0x51, 0xc7, 0x67, 0x3c, 0xbc, 0x13, 0x5a, 0xd8, 0x76, 0x31,
+	0xf3, 0xe8, 0x91, 0x1a, 0x84, 0x77, 0x82, 0xb5, 0x71, 0x27, 0xa7, 0x36, 0xb4, 0x5c, 0x11, 0x25,
+	0x29, 0x66, 0xda, 0x2e, 0x53, 0xdf, 0xb7, 0x0d, 0x39, 0xaf, 0xa1, 0xf9, 0x38, 0x1c, 0x05, 0x30,
+	0x87, 0x6c, 0x34, 0xb8, 0x1e, 0x75, 0x2a, 0x2a, 0xbe, 0x18, 0x5d, 0x8e, 0xae, 0x47, 0x1d, 0xe2,
+	0x8c, 0xa1, 0x75, 0x1e, 0x0a, 0x37, 0x98, 0x2e, 0x16, 0x19, 0xca, 0x7f, 0xfc, 0xa2, 0x23, 0x30,
+	0x85, 0xe6, 0xb4, 0x06, 0x07, 0xac, 0xcc, 0x54, 0x65, 0x88, 0x71, 0xf9, 0x4e, 0x15, 0x3a, 0x5f,
+	0x00, 0xae, 0xf9, 0x3c, 0xc4, 0x71, 0xec, 0xe1, 0x3d, 0x7d, 0x0b, 0xf5, 0xa2, 0x72, 0x63, 0xc4,
+	0xa1, 0x7a, 0xd4, 0xd6, 0x5d, 0x6c, 0xc3, 0xd3, 0x57, 0xb0, 0x3f, 0x0f, 0x85, 0x88, 0x66, 0x0b,
+	0x3f, 0x94, 0x98, 0x96, 0xff, 0xb5, 0xa5, 0xb1, 0x8f, 0x1a, 0x72, 0x04, 0x34, 0x86, 0x4b, 0x74,
+	0x83, 0x6c, 0x15, 0xd1, 0x63, 0xa8, 0x69, 0xad, 0x88, 0xd6, 0xea, 0x48, 0xb5, 0xdd, 0x70, 0x7d,
+	0x25, 0x4d, 0xea, 0xcb, 0x65, 0xc4, 0x74, 0x8d, 0x9a, 0x32, 0x5b, 0x45, 0xba, 0x63, 0x8d, 0xa9,
+	0xd0, 0x79, 0x03, 0xcd, 0xc7, 0xa2, 0x42, 0x95, 0xe1, 0xd9, 0xe9, 0xb0, 0x53, 0xa1, 0xfb, 0xd0,
+	0xb8, 0xbd, 0xfd, 0xc4, 0xb3, 0xe5, 0xfb, 0x77, 0x1d, 0xe2, 0xb8, 0x50, 0xbf, 0xe0, 0x92, 0x4f,
+	0x70, 0xbd, 0xe5, 0x1e, 0xd9, 0x76, 0x8f, 0x42, 0xcd, 0xe3, 0x92, 0x97, 0xd3, 0xea, 0x58, 0x7d,
+	0x1e, 0x3f, 0x2f, 0xb7, 0xca, 0xf0, 0x73, 0xb5, 0x35, 0x6e, 0x8a, 0x5c, 0xa2, 0xa7, 0xb6, 0x46,
+	0x99, 0x5f, 0x65, 0xcd, 0x12, 0x19, 0xc8, 0xe3, 0xa7, 0xd0, 0xde, 0x75, 0x99, 0xd6, 0xa1, 0xca,
+	0x31, 0xeb, 0x54, 0xce, 0x3b, 0xdf, 0x1e, 0xba, 0xe4, 0xfb, 0x43, 0x97, 0xfc, 0x7c, 0xe8, 0x92,
+	0xaf, 0xbf, 0xba, 0x95, 0xb9, 0xa9, 0x57, 0xfe, 0xec, 0x4f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x5d,
+	0x5f, 0x6a, 0x47, 0xfe, 0x03, 0x00, 0x00,
 }
 
 func (m *KV) Marshal() (dAtA []byte, err error) {
@@ -727,6 +738,16 @@ func (m *KV) MarshalTo(dAtA []byte) (int, error) {
 		dAtA[i] = 0x50
 		i++
 		i = encodeVarintPb(dAtA, i, uint64(m.StreamId))
+	}
+	if m.StreamDone {
+		dAtA[i] = 0x58
+		i++
+		if m.StreamDone {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i++
 	}
 	if m.XXX_unrecognized != nil {
 		i += copy(dAtA[i:], m.XXX_unrecognized)
@@ -1041,6 +1062,9 @@ func (m *KV) Size() (n int) {
 	if m.StreamId != 0 {
 		n += 1 + sovPb(uint64(m.StreamId))
 	}
+	if m.StreamDone {
+		n += 2
+	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
 	}
@@ -1202,14 +1226,7 @@ func (m *DataKey) Size() (n int) {
 }
 
 func sovPb(x uint64) (n int) {
-	for {
-		n++
-		x >>= 7
-		if x == 0 {
-			break
-		}
-	}
-	return n
+	return (math_bits.Len64(x|1) + 6) / 7
 }
 func sozPb(x uint64) (n int) {
 	return sovPb(uint64((x << 1) ^ uint64((int64(x) >> 63))))
@@ -1436,6 +1453,26 @@ func (m *KV) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
+		case 11:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field StreamDone", wireType)
+			}
+			var v int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPb
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				v |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			m.StreamDone = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPb(dAtA[iNdEx:])

luxfi/zapdb

This PR introduces, a way to tell StreamWriter to close a stream.
Previously Streams were always open until Flush is called on
StreamWriter. This resulted in memory utilisation, because of underlying
TableBuilder to a sortedWriter. Also closing all sorted writer in single
call resulted in more memory allocation(during Flush()). This can be
useful in some case such as bulk loader in
(cherry picked from commit 700e8124ff6c0eba818b5a5622cbdc4ebda7f5aa)

pb/pb.pb.go
@@ -55,7 +55,9 @@ type KV struct {
 	ExpiresAt uint64 `protobuf:"varint,5,opt,name=expires_at,json=expiresAt,proto3" json:"expires_at,omitempty"`
 	Meta      []byte `protobuf:"bytes,6,opt,name=meta,proto3" json:"meta,omitempty"`
 	// Stream id is used to identify which stream the KV came from.
-	StreamId             uint32   `protobuf:"varint,10,opt,name=stream_id,json=streamId,proto3" json:"stream_id,omitempty"`
+	StreamId uint32 `protobuf:"varint,10,opt,name=stream_id,json=streamId,proto3" json:"stream_id,omitempty"`
+	// Stream done is used to indicate end of stream.
+	StreamDone           bool     `protobuf:"varint,11,opt,name=stream_done,json=streamDone,proto3" json:"stream_done,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -141,6 +143,13 @@ func (m *KV) GetStreamId() uint32 {
 		return m.StreamId
 	}
 	return 0
+}
+
+func (m *KV) GetStreamDone() bool {
+	if m != nil {
+		return m.StreamDone
+	}
+	return false
 }
 
 type KVList struct {
@@ -320,30 +329,31 @@ func init() {
 func init() { proto.RegisterFile("pb.proto", fileDescriptor_f80abaa17e25ccc8) }
 
 var fileDescriptor_f80abaa17e25ccc8 = []byte{
-	// 365 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x91, 0x4f, 0x8a, 0xdb, 0x30,
-	0x14, 0xc6, 0x47, 0x8a, 0xc7, 0xe3, 0xbc, 0xce, 0x04, 0x57, 0x94, 0x22, 0xfa, 0xc7, 0x18, 0x77,
-	0xe3, 0xc5, 0xe0, 0xc5, 0xf4, 0x04, 0x69, 0xea, 0x45, 0x48, 0x42, 0x40, 0x0d, 0xd9, 0x06, 0x39,
-	0x7e, 0x6d, 0x8c, 0x13, 0x5b, 0x58, 0x8a, 0x69, 0x6f, 0xd2, 0x0b, 0xf4, 0x04, 0xbd, 0x44, 0x97,
-	0x3d, 0x42, 0x49, 0x2f, 0x52, 0xac, 0xfc, 0x81, 0xd0, 0xdd, 0xfb, 0xbe, 0xef, 0xbd, 0x4f, 0xf0,
-	0x13, 0x78, 0x2a, 0x4b, 0x54, 0x53, 0x9b, 0x9a, 0x51, 0x95, 0x45, 0x3f, 0x09, 0xd0, 0xc9, 0x92,
-	0xf9, 0xd0, 0x2b, 0xf1, 0x1b, 0x27, 0x21, 0x89, 0xef, 0x45, 0x37, 0xb2, 0x17, 0x70, 0xdb, 0xca,
-	0xed, 0x1e, 0x39, 0xb5, 0xde, 0x51, 0xb0, 0xd7, 0xd0, 0xdf, 0x6b, 0x6c, 0x56, 0x3b, 0x34, 0x92,
-	0xf7, 0x6c, 0xe2, 0x75, 0xc6, 0x0c, 0x8d, 0x64, 0x1c, 0xee, 0x5a, 0x6c, 0x74, 0x51, 0x57, 0xdc,
-	0x09, 0x49, 0xec, 0x88, 0xb3, 0x64, 0x6f, 0x01, 0xf0, 0xab, 0x2a, 0x1a, 0xd4, 0x2b, 0x69, 0xf8,
-	0xad, 0x0d, 0xfb, 0x27, 0x67, 0x68, 0x18, 0x03, 0xc7, 0x16, 0xba, 0xb6, 0xd0, 0xce, 0xdd, 0x4b,
-	0xda, 0x34, 0x28, 0x77, 0xab, 0x22, 0xe7, 0x10, 0x92, 0xf8, 0x41, 0x78, 0x47, 0x63, 0x9c, 0x47,
-	0x21, 0xb8, 0x93, 0xe5, 0xb4, 0xd0, 0x86, 0xbd, 0x04, 0x5a, 0xb6, 0x9c, 0x84, 0xbd, 0xf8, 0xd9,
-	0x93, 0x9b, 0xa8, 0x2c, 0x99, 0x2c, 0x05, 0x2d, 0xdb, 0x68, 0x08, 0xcf, 0x67, 0xb2, 0x2a, 0x3e,
-	0xa3, 0x36, 0xa3, 0x8d, 0xac, 0xbe, 0xe0, 0x27, 0x34, 0xec, 0x11, 0xee, 0xd6, 0x56, 0xe8, 0xd3,
-	0x05, 0xeb, 0x2e, 0xae, 0xf7, 0xc4, 0x79, 0x25, 0xfa, 0x41, 0x60, 0x70, 0x9d, 0xb1, 0x01, 0xd0,
-	0x71, 0x6e, 0x29, 0x39, 0x82, 0x8e, 0x73, 0xf6, 0x08, 0x74, 0xae, 0x2c, 0xa1, 0xc1, 0xd3, 0x9b,
-	0xff, 0xbb, 0x92, 0xb9, 0xc2, 0x46, 0x9a, 0xa2, 0xae, 0x04, 0x9d, 0xab, 0x0e, 0xe9, 0x14, 0x5b,
-	0xdc, 0x5a, 0x70, 0x0f, 0xe2, 0x28, 0xd8, 0x2b, 0xf0, 0x46, 0x1b, 0x5c, 0x97, 0x7a, 0xbf, 0xb3,
-	0xd8, 0xee, 0xc5, 0x45, 0x47, 0xef, 0xa0, 0x7f, 0xa9, 0x60, 0x00, 0xee, 0x48, 0xa4, 0xc3, 0x45,
-	0xea, 0xdf, 0x74, 0xf3, 0xc7, 0x74, 0x9a, 0x2e, 0x52, 0x9f, 0x7c, 0xf0, 0x7f, 0x1d, 0x02, 0xf2,
-	0xfb, 0x10, 0x90, 0x3f, 0x87, 0x80, 0x7c, 0xff, 0x1b, 0xdc, 0x64, 0xae, 0xfd, 0xdf, 0xf7, 0xff,
-	0x02, 0x00, 0x00, 0xff, 0xff, 0xeb, 0x28, 0x5d, 0xcf, 0xeb, 0x01, 0x00, 0x00,
+	// 384 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x92, 0x4f, 0x6a, 0xdb, 0x40,
+	0x14, 0xc6, 0x33, 0x63, 0x47, 0x91, 0x9f, 0x13, 0xe3, 0x3e, 0x4a, 0x19, 0xfa, 0xc7, 0x15, 0xea,
+	0x46, 0x8b, 0xa0, 0x45, 0x7a, 0x02, 0xd7, 0xd1, 0xc2, 0xd8, 0xc1, 0x30, 0x0d, 0xde, 0x9a, 0x51,
+	0xf4, 0xda, 0x08, 0xc5, 0xd2, 0xa0, 0x19, 0x8b, 0xf6, 0x26, 0xbd, 0x40, 0xef, 0xd2, 0x65, 0x17,
+	0x3d, 0x40, 0x71, 0x2f, 0x52, 0x34, 0x72, 0x02, 0x26, 0xbb, 0xf7, 0x7d, 0xdf, 0x7b, 0xdf, 0xc0,
+	0x8f, 0x01, 0x5f, 0xa7, 0xb1, 0xae, 0x2b, 0x5b, 0x21, 0xd7, 0x69, 0xf8, 0x87, 0x01, 0x5f, 0xac,
+	0x71, 0x0c, 0xbd, 0x82, 0xbe, 0x0b, 0x16, 0xb0, 0xe8, 0x5c, 0xb6, 0x23, 0xbe, 0x84, 0xd3, 0x46,
+	0x3d, 0xec, 0x48, 0x70, 0xe7, 0x75, 0x02, 0xdf, 0xc0, 0x60, 0x67, 0xa8, 0xde, 0x6c, 0xc9, 0x2a,
+	0xd1, 0x73, 0x89, 0xdf, 0x1a, 0x37, 0x64, 0x15, 0x0a, 0x38, 0x6b, 0xa8, 0x36, 0x79, 0x55, 0x8a,
+	0x7e, 0xc0, 0xa2, 0xbe, 0x7c, 0x94, 0xf8, 0x0e, 0x80, 0xbe, 0xe9, 0xbc, 0x26, 0xb3, 0x51, 0x56,
+	0x9c, 0xba, 0x70, 0x70, 0x70, 0xa6, 0x16, 0x11, 0xfa, 0xae, 0xd0, 0x73, 0x85, 0x6e, 0x6e, 0x5f,
+	0x32, 0xb6, 0x26, 0xb5, 0xdd, 0xe4, 0x99, 0x80, 0x80, 0x45, 0x17, 0xd2, 0xef, 0x8c, 0x79, 0x86,
+	0xef, 0x61, 0x78, 0x08, 0xb3, 0xaa, 0x24, 0x31, 0x0c, 0x58, 0xe4, 0x4b, 0xe8, 0xac, 0xeb, 0xaa,
+	0xa4, 0x30, 0x00, 0x6f, 0xb1, 0x5e, 0xe6, 0xc6, 0xe2, 0x2b, 0xe0, 0x45, 0x23, 0x58, 0xd0, 0x8b,
+	0x86, 0x57, 0x5e, 0xac, 0xd3, 0x78, 0xb1, 0x96, 0xbc, 0x68, 0xc2, 0x29, 0xbc, 0xb8, 0x51, 0x65,
+	0xfe, 0x85, 0x8c, 0x9d, 0xdd, 0xab, 0xf2, 0x2b, 0x7d, 0x26, 0x8b, 0x97, 0x70, 0x76, 0xe7, 0x84,
+	0x39, 0x5c, 0x60, 0x7b, 0x71, 0xbc, 0x27, 0x1f, 0x57, 0xc2, 0x9f, 0x0c, 0x46, 0xc7, 0x19, 0x8e,
+	0x80, 0xcf, 0x33, 0x87, 0xb1, 0x2f, 0xf9, 0x3c, 0xc3, 0x4b, 0xe0, 0x2b, 0xed, 0x10, 0x8e, 0xae,
+	0xde, 0x3e, 0xef, 0x8a, 0x57, 0x9a, 0x6a, 0x65, 0xf3, 0xaa, 0x94, 0x7c, 0xa5, 0x5b, 0xe6, 0x4b,
+	0x6a, 0xe8, 0xc1, 0x91, 0xbd, 0x90, 0x9d, 0xc0, 0xd7, 0xe0, 0xcf, 0xee, 0xe9, 0xae, 0x30, 0xbb,
+	0xad, 0xe3, 0x7a, 0x2e, 0x9f, 0x74, 0xf8, 0x01, 0x06, 0x4f, 0x15, 0x08, 0xe0, 0xcd, 0x64, 0x32,
+	0xbd, 0x4d, 0xc6, 0x27, 0xed, 0x7c, 0x9d, 0x2c, 0x93, 0xdb, 0x64, 0xcc, 0x3e, 0x8d, 0x7f, 0xed,
+	0x27, 0xec, 0xf7, 0x7e, 0xc2, 0xfe, 0xee, 0x27, 0xec, 0xc7, 0xbf, 0xc9, 0x49, 0xea, 0xb9, 0x0f,
+	0xf0, 0xf1, 0x7f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x50, 0x3d, 0x49, 0xb9, 0x0c, 0x02, 0x00, 0x00,
 }
 
 func (m *KV) Marshal() (dAtA []byte, err error) {
@@ -369,6 +379,16 @@ func (m *KV) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	if m.XXX_unrecognized != nil {
 		i -= len(m.XXX_unrecognized)
 		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if m.StreamDone {
+		i--
+		if m.StreamDone {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i--
+		dAtA[i] = 0x58
 	}
 	if m.StreamId != 0 {
 		i = encodeVarintPb(dAtA, i, uint64(m.StreamId))
@@ -588,6 +608,9 @@ func (m *KV) Size() (n int) {
 	}
 	if m.StreamId != 0 {
 		n += 1 + sovPb(uint64(m.StreamId))
+	}
+	if m.StreamDone {
+		n += 2
 	}
 	if m.XXX_unrecognized != nil {
 		n += len(m.XXX_unrecognized)
@@ -884,6 +907,26 @@ func (m *KV) Unmarshal(dAtA []byte) error {
 					break
 				}
 			}
+		case 11:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field StreamDone", wireType)
+			}
+			var v int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPb
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				v |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			m.StreamDone = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPb(dAtA[iNdEx:])